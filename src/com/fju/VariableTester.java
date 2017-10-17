68a9153d5d0c3efab1cd82ce7181ca1e2a0ca738
andy0521 <andy132888@gmail.com>
2017-10-17 16:37:01 +0800

    variable, primitive

---
+++ src/com/fju/VariableTester.java
@@ -0,0 +1,37 @@
+package com.fju;
+
+import java.util.Random;
+
+public class VariableTester {
+
+	public static void main(String[] args) 
+{
+		int age =0;
+		float weight =/*(float)*/69.5f;
+		char c =65;
+		char c2 = 'z';
+		int i =c2;
+		System.out.println((int)c2);
+		
+		System.out.println(i);
+
+		System.out.println(c2);
+		System.out.println(c);
+		System.out.println(weight);
+		boolean pass =false;
+		System.out.println(!pass);
+		System.out.println(pass);
+		String s =new String("hello");
+		Random r = new Random();
+		int n1 = r.nextInt(6)+1;
+		int n2 = r.nextInt(6)+1;
+		int n3 = r.nextInt(6)+1;
+		int n4 = r.nextInt(6)+1;
+		System.out.println(3 <= 2);
+		
+	
+				
+				
+}
+
+}
