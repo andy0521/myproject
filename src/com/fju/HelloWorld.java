d040bede2cb239021ee8a42e0298412022867543
andy0521 <andy132888@gmail.com>
2017-10-24 16:46:45 +0800

    ch3

--- src/com/fju/HelloWorld.java
+++ src/com/fju/HelloWorld.java
@@ -1,9 +1,20 @@
 package com.fju;
 
+import java.util.Scanner;
+
 public class HelloWorld {
      public static void main(String[]args){
-    	 System.out.println("Hello World");
-    	Person p = new Person();
-    	 p.sayHello();
+    	 System.out.println("請輸入你的名字");
+    	/*Person p = new Person();
+    	 p.sayHello();*/
+    	 Scanner scanner = new Scanner(System.in);
+        String line = scanner.nextLine();
+        System.out.println("Hello," + line);
+        System.out.println("你幾歲");
+        line = scanner.nextLine();
+        int age = Integer.parseInt(line);
+        System.out.println(age);
+        System.out.println(age > 25);
+
      }
 }
--- src/com/fju/VariableTester.java
+++ src/com/fju/VariableTester.java
@@ -21,17 +21,34 @@ public class VariableTester {
 		boolean pass =false;
 		System.out.println(!pass);
 		System.out.println(pass);
-		String s =new String("hello");
+		
+		
 		Random r = new Random();
 		int n1 = r.nextInt(6)+1;
 		int n2 = r.nextInt(6)+1;
 		int n3 = r.nextInt(6)+1;
 		int n4 = r.nextInt(6)+1;
-		System.out.println(3 <= 2);
-		
 	
+		System.out.println(3 <= 2);
+		//算
+		String s =new String("Hello");
+		int len = s.length();
+		System.out.println("len" + len);
+		System.out.println(s.charAt(0));
+		  float f = 6.7f;
+	        int x = 5;
+	        int y = 3;
+	        int z = x % y;
+	        System.out.println(z);
+	        System.out.println(3>>1);
+	        System.out.println(3<<1);
+	       x++;// x= x+1
+	        x--; // x=x-1
+	        System.out.println(x);
+	        x+=2;
+	        System.out.println(x);
+	     }
 				
 				
 }
 
-}
