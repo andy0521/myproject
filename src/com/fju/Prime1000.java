b65aaf812ff0ede9650349b8e184f46fec218a89
andy0521 <andy132888@gmail.com>
2017-11-07 15:59:20 +0800

    niner

--- src/com/fju/Prime1000.java
+++ src/com/fju/Prime1000.java
@@ -7,16 +7,16 @@ public class Prime1000 {
 		
 		for (int n = 2;n<=1000; n++)
 		{
-			boolean prime =false;
+			boolean prime =true;
 			for (int i=2; i<n;i++){
 				if( n%i==0){
 					prime = false ;
 					break;}
-				if ( prime =true){
-					System.out.print(n+" ");
 				
-			}
-		}
-			}
-		}
-}
+				
+			}if ( prime){
+			System.out.print(n+"\t");}
+		}}}
+			
+		
+
