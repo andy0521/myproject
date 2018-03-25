2df90515b5a98ff581f7506309831a0eabd8b19f
andy0521 <andy132888@gmail.com>
2018-03-25 10:34:44 +0800

    abgame

--- src/com/java2/object/ABTGame.java
+++ src/com/java2/object/ABTGame.java
@@ -10,9 +10,15 @@ public class ABTGame {
 		Scanner sc1 =new Scanner(System.in);
 		int result =0;
 		while (result !=40){
-			System.out.print("please enter your answer");
-		
+			
+				System.out.print("Please enter your guess:");
+				ABNumber n = new ABNumber(sc1.nextLine());
+				result = n.validate(secret);
+				System.out.println((result/10)+"A"+(result%10)+"B");
+			}
 		}
+
+	
+
 	}
 
-}
