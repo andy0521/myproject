0425667f62241e012158e0ccb345edb2f1a731fe
andy0521 <andy132888@gmail.com>
2018-03-30 19:23:31 +0800

    poker2

--- src/com/java2/object/Poker2.java
+++ src/com/java2/object/Poker2.java
@@ -1,41 +1,49 @@
 package com.java2.object;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
 
+
+import java.util.Random;
+import java.util.*;
 public class Poker2 {
-	public class Poker {
-		Random random = new Random();
-//		int[] cards = new int[52];
-		ArrayList<Integer> cards = new ArrayList<>(); 
+	
+		Random r1 = new Random();
+		// int[] cards = new int[52];
+		 List<Integer> cards = new ArrayList<>();
+		 String flowers = "SHDC";
+
+		public void poker() {
+			for (int i = 0; i < 52; i++) {
+				cards.add(i);
+				}
+		}
 		
-		String flowers = "SHDC";
-		public Poker() {
 			
-			for (int i = 0; i < cards.length; i++) {
-				cards[i] = i;
-				System.out.println((i % 13) + 1 + "" + (flowers.charAt(i / 13)));
+	
+		public void shuffle() {
+			for (int i = 0; i < cards.size(); i++) {
+				int r = r1.nextInt(51);
+				int temp = cards.get(i);
+				cards.set(i, cards.get(r));
+				cards.set(r, temp);
+
+				//// cards[i] <-> cards[r]
+				// int temp = cards[i];
+				// cards[i] = cards[r];
+				// cards[r] = temp;
 			}
 		}
-		
-		public void shuffle(){
-			for (int i=0; i<cards.length; i++){
-				int r =random.nextInt(52);
-//				cards[i] <-> cards[r]
-				int temp = cards[i];
-				cards[i] = cards[r];
-				cards[r] = temp;
-			}
-		}
-		
-		public void show(){
-			for (int i=0; i<cards.length; i++){
-				int c = cards[i];
-				System.out.print((c%13)+1 + "" + (flowers.charAt(c/13)) + " ");
-				if (i%13==0){
+
+		public void show() {
+			for (int i = 0; i < cards.size(); i++) {
+				int c = cards.get(i);
+
+				System.out.print((c % 13) + 1 + "" + (flowers.charAt(c / 13)) + " ");
+				if (i % 13 == 0) {
 					System.out.println();
 				}
 			}
 		}
-	}}
+	}
+
+
+	
--- src/com/java2/object/PokerTester.java
+++ src/com/java2/object/PokerTester.java
@@ -1,16 +1,12 @@
 package com.java2.object;
-
-import java.util.Random;
-
+import java.util.*;
 public class PokerTester {
 
 	public static void main(String[] args) {
+		// TODO 自動產生的方法 Stub
+		Poker2 p1 = new Poker2();
+		p1.poker();
+		p1.shuffle();
+			p1.show();
 		
-		String flowers = "SHDC";
-		Poker poker = new Poker();
-	
-	}
-
-}
-
-
+}}
