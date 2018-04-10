f7cd61453dbd36079fa2652784f96431d5e921a6
andy0521 <andy132888@gmail.com>
2018-04-10 16:28:20 +0800

    file output stream

---
+++ src/com/java2/object/FileInputStream.java
@@ -0,0 +1,25 @@
+package com.java2.object;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class FileInputStream {
+
+	public static void main(String[] args) {
+		try{
+			// TODO Auto-generated method stub
+		FileOutputStream out = new FileOutputStream("data.txt");
+		
+		out.write(65);
+		out.flush();
+		out.close();
+		}catch ( FileNotFoundException e) {
+			System.out.print("file not found");
+			// TODO: handle exception
+		
+	}catch (IOException e) {
+		// TODO: handle exception
+		System.out.print("file output problem");
+	}
+	}
+	}
---
+++ src/com/java2/object/FileTest.java
@@ -0,0 +1,13 @@
+package com.java2.object;
+
+public class FileTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		try {
+			FileInputStream out = new FileInputStream("data.txt");
+			
+		}catch
+	}
+
+}
