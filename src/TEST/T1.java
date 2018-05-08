0a5bef564aa4e271a0f17ae18e4d7fe2fe25a644
andy0521 <andy132888@gmail.com>
2018-05-08 16:09:00 +0800

    2018/05/08

---
+++ src/TEST/T1.java
@@ -0,0 +1,13 @@
+package TEST;
+import java.util.*;
+
+public class T1 {
+
+  public static void main(String[] args) {
+List<Integer> numList = new ArrayList<>(Arrays.asList(2,0,1,7));
+
+    //write your code here
+
+
+    System.out.println(numList);
+  }}
