6652eb4fd240a1a740f591cbcc99559c03aa1553
andy0521 <andy132888@gmail.com>
2018-05-15 16:22:21 +0800

    20180515

--- src/threading/Horse.java
+++ src/threading/Horse.java
@@ -4,8 +4,14 @@ public class Horse extends Thread {
 	@Override
 	public void run(){
 		
-		for (int i=0;i<=50000;i++){
+		for (int i=0;i<=100;i++){
 			System.out.println (getName()+":"+i);
+			try {
+				sleep(50);
+			}catch (InterruptedException e) {
+				// TODO: handle exception
+				e.printStackTrace();
+			}
 		}
 	}
 	
--- src/threading/Racing.java
+++ src/threading/Racing.java
@@ -1,14 +1,22 @@
 package threading;
 
+import java.util.List;
+import java.util.ArrayList;
+
 public class Racing {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Horse h1 = new Horse();
-		Horse h2 = new Horse();
-		h1.start();
-		h2.start();
+		List <Horse> horses=new ArrayList<>();
+		for (int i=0 ;i<=8;i++){
+			
+		
+		Horse h = new Horse();
+	horses.add(h);
 
+		h.start();
+
+		}
 	}
 
 }
