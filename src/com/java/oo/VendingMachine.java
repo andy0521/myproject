093f6290b014baaee65a58f3ff2d934783ebda7f
andy0521 <andy132888@gmail.com>
2018-06-07 21:18:35 +0800

    20180607

--- src/com/java/oo/VendingMachine.java
+++ src/com/java/oo/VendingMachine.java
@@ -2,12 +2,26 @@ package com.java.oo;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class VendingMachine {
 	int total = 0;
-
-List<Drink> drinks =  new ArrayList<>();
-public void on (){
-	System.out.println();
-}
+	List<Drink> drinks = new ArrayList<>();
+	
+	public void on(){
+		System.out.println("Please insert coins...");
+		for (int i=0; i<drinks.size(); i++){
+			Drink drink = drinks.get(i);
+			System.out.println(drink.getId() + "\t" + drink.getName() + "\t" + drink.getPrice());
+		}
+		Scanner scanner = new Scanner(System.in);
+		int data = -1;
+		while (data != 0){
+			data = scanner.nextInt();
+			switch(data){
+			case 0:
+				break;
+			}
+		}
+	}
 }
--- src/com/java/oo/VendingMain.java
+++ src/com/java/oo/VendingMain.java
@@ -8,31 +8,29 @@ import java.io.IOException;
 public class VendingMain {
 
 	public static void main(String[] args) {
-
-		// TODO Auto-generated method stub
 		VendingMachine vm = new VendingMachine();
-		FileReader fr = null ;
+		FileReader fr;
 		try {
+			fr = new FileReader("vending.txt");
 			BufferedReader in = new BufferedReader(fr);
 			String line = in.readLine();
-			String[] token = line.split(",");
-			line = in.readLine();
-			int count = Integer.parseInt(token[0]);
-			for (int i = 0; i < count; i++) {
-				String name = token[i * 2 + 1];
-				int price = Integer.parseInt(token[i * 2 + 2]);
-				vm.drinks.add(new Drink(i + 1, name, price));
+			String[] tokens = line.split(",");
+			int count = Integer.parseInt(tokens[0]);
+			for (int i = 0; i < count; i++){
+				String name = tokens[i*2+1];
+				int price = Integer.parseInt(tokens[i*2+2]);
+				vm.drinks.add(new Drink(i+1, name, price));
 			}
 			vm.on();
+			
 		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-
 		}
-
-		// TODO: handle exception
+		
 	}
 
 }
