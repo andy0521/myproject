024922b762df2b88c7f88ee21537dd69460cdc09
andy0521 <andy132888@gmail.com>
2018-06-08 22:14:05 +0800

    20180608

--- src/com/java/oo/VendingMachine.java
+++ src/com/java/oo/VendingMachine.java
@@ -5,11 +5,12 @@ import java.util.List;
 import java.util.Scanner;
 
 public class VendingMachine {
-	int total = 0;
+	
 	List<Drink> drinks = new ArrayList<>();
 	
 	public void on(){
-		System.out.println("Please insert coins...");
+		int total = 0;
+		
 		for (int i=0; i<drinks.size(); i++){
 			Drink drink = drinks.get(i);
 			System.out.println(drink.getId() + "\t" + drink.getName() + "\t" + drink.getPrice());
@@ -17,9 +18,21 @@ public class VendingMachine {
 		Scanner scanner = new Scanner(System.in);
 		int data = -1;
 		while (data != 0){
+			System.out.println("Please insert coins...");
+			System.out.println("餘額:"+total);
 			data = scanner.nextInt();
 			switch(data){
 			case 0:
+				data=0;
+				break;
+			case 5:
+				total=total+5;
+				break;
+			case 10:
+				total=total+10;
+				break;
+			case 50:
+				total=total+50;
 				break;
 			}
 		}
